d37d215453d1e138f4e67e831a4c971350d10bc4
Shuaib Albastaki <stargazingwiz@gmail.com>
2022-09-18 12:37:58 +0400

    additional design changes

---
+++ lib/design/frosted_card.dart
@@ -0,0 +1,64 @@
+// ignore_for_file: prefer_typing_uninitialized_variables
+
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class FrostedCard extends StatelessWidget {
+  final theWidth;
+  final theHeight;
+  final theChild;
+
+  const FrostedCard({
+    Key? key,
+    required this.theWidth,
+    required this.theHeight,
+    required this.theChild,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(30),
+      child: Container(
+        width: theWidth,
+        height: theHeight,
+        color: Colors.greenAccent.withOpacity(0.1),
+        child: Stack(
+          children: [
+            //blur effect ==> the third layer of stack
+            BackdropFilter(
+              filter: ImageFilter.blur(
+                //sigmaX is the Horizontal blur
+                sigmaX: 4.0,
+                //sigmaY is the Vertical blur
+                sigmaY: 4.0,
+              ),
+              //we use this container to scale up the blur effect to fit its
+              //  parent, without this container the blur effect doesn't appear.
+              child: Container(),
+            ),
+            //gradient effect ==> the second layer of stack
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                border: Border.all(color: Colors.white.withOpacity(0.13)),
+                gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [
+                      //begin color
+                      Colors.white.withOpacity(0.15),
+                      //end color
+                      Colors.white.withOpacity(0.05),
+                    ]),
+              ),
+            ),
+            //child ==> the first/top layer of stack
+            Center(child: theChild),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/navigation/welcome_page.dart
+++ lib/navigation/welcome_page.dart
@@ -4,10 +4,31 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:pomo_timer/design/clicky_button.dart';
+import 'package:pomo_timer/design/frosted_card.dart';
 import 'package:pomo_timer/navigation/home_page.dart';
+import 'package:video_player/video_player.dart';
 
-class WelcomePage extends StatelessWidget {
+class WelcomePage extends StatefulWidget {
   const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  late VideoPlayerController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = VideoPlayerController.asset("assets/forest.mp4")
+      ..initialize().then((_) {
+        setState(() {});
+      });
+    controller.setLooping(true);
+    controller.play();
+    controller.setVolume(0);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,90 +39,115 @@ class WelcomePage extends StatelessWidget {
         backgroundColor: Colors.transparent,
         elevation: 0,
       ),
-      body: Padding(
-        padding: EdgeInsets.all(18),
-        child: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                RichText(
-                  textAlign: TextAlign.left,
-                  text: TextSpan(
-                    style: GoogleFonts.fjallaOne(
-                      textStyle: TextStyle(
-                          fontSize: 48, color: Colors.lightGreen.shade800),
-                    ),
-                    children: <TextSpan>[
-                      TextSpan(
-                        text: "How ",
-                        style: TextStyle(fontWeight: FontWeight.w600),
-                      ),
-                      TextSpan(
-                        text: "many ",
-                        style: TextStyle(fontWeight: FontWeight.w600),
-                      ),
-                      TextSpan(
-                        text: "pomodoro ",
-                        style: TextStyle(
-                            fontWeight: FontWeight.w600,
-                            color: Colors.red.shade600),
-                      ),
-                      TextSpan(
-                        text: "cycles",
-                        style: TextStyle(
-                            fontWeight: FontWeight.w600,
-                            decoration: TextDecoration.underline),
-                      ),
-                      TextSpan(
-                        text: "?",
-                        style: TextStyle(fontWeight: FontWeight.w600),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Enter break cycles here',
-                  ),
-                  keyboardType: TextInputType.number,
-                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                ),
-                SizedBox(
-                  height: 40,
-                ),
-                Transform.scale(
-                  scale: 0.8,
-                  child: ClickyButton(
-                      child: Text(
-                        "I'm ready",
-                        style: GoogleFonts.dosis(
-                          textStyle: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 40,
-                            fontWeight: FontWeight.bold,
+      body: Stack(
+        children: [
+          VideoPlayer(controller),
+          Center(
+            child: FrostedCard(
+              theHeight: 320.0,
+              theWidth: 380.0,
+              theChild: Padding(
+                padding: EdgeInsets.all(18),
+                child: Center(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        RichText(
+                          textAlign: TextAlign.left,
+                          text: TextSpan(
+                            style: GoogleFonts.fjallaOne(
+                              textStyle: TextStyle(
+                                  fontSize: 48,
+                                  color: Colors.lightGreen.shade200),
+                            ),
+                            children: <TextSpan>[
+                              TextSpan(
+                                text: "How ",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                              TextSpan(
+                                text: "many ",
+                                style: TextStyle(fontWeight: FontWeight.w600),
+                              ),
+                              TextSpan(
+                                text: "pomodoro ",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w600,
+                                    color: Colors.red.shade400),
+                              ),
+                              TextSpan(
+                                text: "cycles",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w600,
+                                    decoration: TextDecoration.underline),
+                              ),
+                              TextSpan(
+                                text: "?",
+                                style: TextStyle(fontWeight: FontWeight.w600),
+                              ),
+                            ],
                           ),
                         ),
-                      ),
-                      color: Colors.teal,
-                      onPressed: () async {
-                        Future.delayed(Duration(milliseconds: 1600), () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => HomePage()),
-                          );
-                        });
-                      }),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'hint: 1 Cycle = 25min + 5min',
+                            filled: true,
+                            fillColor: Colors.white.withOpacity(1),
+                          ),
+                          keyboardType: TextInputType.number,
+                          inputFormatters: [
+                            FilteringTextInputFormatter.digitsOnly
+                          ],
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Transform.scale(
+                          scale: 0.8,
+                          child: ClickyButton(
+                              child: Text(
+                                "I'm ready",
+                                style: GoogleFonts.dosis(
+                                  textStyle: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 40,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                              color: Colors.teal,
+                              onPressed: () async {
+                                Future.delayed(Duration(milliseconds: 1600),
+                                    () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => HomePage()),
+                                  );
+                                });
+                              }),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
-              ],
+              ),
             ),
           ),
-        ),
+        ],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
   }
 }
