7f27fd37f4187c7d3d0711b29e78589350e2472f
Shuaib Albastaki <stargazingwiz@gmail.com>
2022-09-23 10:13:58 +0400

    removed back button on HomePage

--- lib/navigation/home_page.dart
+++ lib/navigation/home_page.dart
@@ -105,6 +105,7 @@ class _HomePageState extends State<HomePage>
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         centerTitle: true,
         backgroundColor: Colors.transparent,
         elevation: 0,
--- lib/navigation/welcome_page.dart
+++ lib/navigation/welcome_page.dart
@@ -1,7 +1,5 @@
 // ignore_for_file: prefer_const_constructors, sort_child_properties_last, prefer_const_literals_to_create_immutables
 
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:pomo_timer/design/clicky_button.dart';
